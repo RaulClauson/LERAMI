8eacfc79339416400309338edd6ffe9b4fe86a24
Raul Clauson <106999644+RaulClauson@users.noreply.github.com>
2024-08-05 17:34:00 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,32 +8,7 @@ import Team from "./Components/Team";
 import Texture from "./Components/Texture";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useEffect } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
-
 function App() {
-
-  useEffect(() => {
-    const elements = gsap.utils.toArray(".ani_visible");
-
-    elements.forEach((element, index) => {
-      gsap.fromTo(element, { opacity: 0, transform: "translateY(70px)" }, {
-        duration: 0.8, 
-        delay: index * 0.1,
-        opacity: 1,
-        transform: "translateY(0px)",
-        ease: "power4.out",
-        scrollTrigger: {
-            trigger: element,
-            start: "top 90%",
-            end: "top 10%",
-            toggleActions: "restart none none reverse",
-        }
-      })
-    })
-  }, [])
 
   return (
     <div className="App">
